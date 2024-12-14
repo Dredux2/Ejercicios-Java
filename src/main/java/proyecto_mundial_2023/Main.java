6be58304ca4534afa82be838c5399b47d5e6d21a
Dredux <David-RD@outlook.es>
2024-12-14 14:27:03 +0100

    Proyecto Navidad Verificacion de Entradas

--- src/main/java/proyecto_mundial_2023/Main.java
+++ src/main/java/proyecto_mundial_2023/Main.java
@@ -1,7 +1,25 @@
 package proyecto_mundial_2023;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*** MUNDIAL FEMENINO 2023 ***");
-        Entradas.verificarEntradas();
+        Scanner sc = new Scanner(System.in);
+        int casos = sc.nextInt();
+        for (int i = 0; i < casos; i++) {
+            int numeroTicket = sc.nextInt();
+            int codigo = codigoVerificacion(numeroTicket);
+            System.out.println(codigo);
+        }
+    }
+
+    public static int codigoVerificacion(int numeroTicket) {
+        int codigo = 0;
+        int digito;
+        int entrada = numeroTicket;
+        while (entrada > 0) {
+            digito = entrada % 10;
+            codigo += (digito * ((numeroTicket % 10) % 2 == 0 ? 3 : 2));
+            entrada /= 10; // Reducir el valor de entrada
+        }
+        return codigo;
     }
 }
