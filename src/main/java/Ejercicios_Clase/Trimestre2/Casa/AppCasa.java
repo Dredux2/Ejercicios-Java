37b874d8a950184a69e88ae110a4d3678e77dfce
Dredux <David-RD@outlook.es>
2025-02-05 17:25:36 +0100

    Casa

---
+++ src/main/java/Ejercicios_Clase/Trimestre2/Casa/AppCasa.java
@@ -0,0 +1,23 @@
+package Ejercicios_Clase.Trimestre2.Casa;
+
+public class AppCasa {
+
+    public static void main(String[] args) {
+
+        Casa casa = new Casa("Calle Mayor 123");
+        System.out.println(casa);
+
+        casa.agregarHabitacion("Dormitorio", 40);
+        casa.agregarHabitacion("Cocina", 65);
+        casa.agregarHabitacion("Baño", 10);
+
+        casa.mostrarHabitaciones();
+
+        Habitacion cocina = casa.mostrarHabitaciones();
+        cocina.agregarElectrodomestico(new Electrodomestico("Refrigerador", 150));
+        cocina.agregarElectrodomestico(new Electrodomestico("Microondas", 1200));
+        cocina.mostrarElectrodomesticos();
+
+        System.out.println("Habitación con mayor consumo: " + casa.habitacionMasConsumo() + " watts");
+    }
+}
