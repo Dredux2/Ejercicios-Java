cb85f0216f484d9ff74ecb174188a5233f15842e
Dredux <David-RD@outlook.es>
2025-03-18 13:59:51 +0100

    Ejercicio 1 Impresora Incompleto

---
+++ src/main/java/Ejercicios_Colecciones/Peticion.java
@@ -0,0 +1,39 @@
+package Ejercicios_Colecciones;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.Objects;
+@Getter @Setter @ToString
+public class Peticion {
+    private String id;
+    private String nombre;
+
+    public Peticion(String nombre){
+        if (!id.matches("^PC\\d{4}$")){
+            throw new RuntimeException("ERROR: Formato de identificador incorrecto");
+        } else {
+            this.id = id;
+        }
+        nombreArchivo(nombre);
+    }
+
+    public void nombreArchivo(String nombre){
+        if (!nombre.toLowerCase().endsWith(".pdf")){
+            throw new RuntimeException("ERROR: El archivo " + nombre + " no es un pdf");
+        } else {
+            this.nombre = nombre;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Peticion peticion = (Peticion) o;
+        return Objects.equals(id, peticion.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
