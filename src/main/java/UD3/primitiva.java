0c27ed6b8328734a2d587fcd39cfbf0dbdc56bdc
Dredux <David-RD@outlook.es>
2025-04-08 14:23:39 +0200

    Improves primitiva game logic
    Improves the primitiva game logic by implementing the
    number generation, matching, and result display
    functionalities.
    It also adds error handling for incorrect ticket formats.

--- src/main/java/UD3/primitiva.java
+++ src/main/java/UD3/primitiva.java
@@ -3,30 +3,50 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 public class primitiva {
-    // Incomplete code
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rm = new Random();
         System.out.print("Introduce los datos de tu boleto: ");
         String boleto = sc.nextLine();
-        String listaNumeros[] = null;
         boolean formato = boleto.matches("\\d{1,49}-\\d{1,49}-\\d{1,49}-\\d{1,49}-\\d{1,49}-\\d{1,49}/\\d{0,9}");
         if (!formato){
-            System.out.println("Formato de boleto incorrecto. Vuelva a intentarlo.");
+            System.out.print("ERROR: Formato de boleto incorrecto");
         } else {
-            listaNumeros = boleto.split("-/");
-        }
+            String listaNumeros[] = boleto.split("[-/]");
+            System.out.println(Arrays.toString(listaNumeros));
 
-        int sorteo[] = new int[6];
-        int complementario = rm.nextInt(1,49);
-        do {
-            for (int i = 0; i < 6; i++){
-                sorteo[i] = rm.nextInt(1,49);
-                sorteo = Arrays.stream(sorteo).distinct().toArray();
-                if (sorteo[i] != complementario){
+            int sorteo[] = new int[6];
+            int complementario = rm.nextInt(1,49);
+            int reintegro = rm.nextInt(9);
+            do {
+                for (int i = 0; i < 6; i++){
                     sorteo[i] = rm.nextInt(1,49);
+                    if (sorteo[i] == complementario){
+                        sorteo[i] = rm.nextInt(1,49);
+                    }
+                }
+                sorteo = Arrays.stream(sorteo).distinct().toArray();
+            } while (sorteo.length < 6);
+
+            System.out.println("\nSORTEO:");
+            System.out.println(Arrays.toString(sorteo));
+            System.out.println("Numero Complementario: " + complementario);
+            System.out.println("Reintegro: " + reintegro);
+
+            System.out.println("\nRESULTADOS:");
+            int coincidencias = 0;
+            for (int i = 0; i < 6; i++){
+                int numJugador = Integer.parseInt(listaNumeros[i]);
+                for (int numBoleto : sorteo){
+                    if (numJugador == numBoleto){
+                        coincidencias++;
+                    }
                 }
             }
-        } while (sorteo.length < 6);
+            System.out.println(coincidencias + " acierto/s");
+            if (Integer.parseInt(listaNumeros[6]) == reintegro){
+                System.out.println("Reintegro");
+            }
+        }
     }
 }
