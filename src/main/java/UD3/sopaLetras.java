bec04751382a3ba315592e4a5eb1276fc7ad61c7
Dredux <David-RD@outlook.es>
2025-04-10 16:24:48 +0200

    Improves input validation and error handling
    Enhances the robustness of the application by implementing comprehensive input validation and error handling mechanisms.
    
    Adds a try-catch block to handle potential InputMismatchException, improving user experience by providing informative error messages.
    Validates user input for row and column dimensions, ensuring data integrity and preventing unexpected program behavior.

--- src/main/java/UD3/sopaLetras.java
+++ src/main/java/UD3/sopaLetras.java
@@ -1,75 +1,60 @@
 package UD3;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class sopaLetras {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Introduce el número de filas: ");
-        int filas = sc.nextInt();
+        try {
+            System.out.print("Introduce el número de filas: ");
+            int filas = sc.nextInt();
 
-        System.out.print("Introduce el número de columnas: ");
-        int columnas = sc.nextInt();
+            System.out.print("Introduce el número de columnas: ");
+            int columnas = sc.nextInt();
 
-        String matriz[][] = new String[filas][columnas];
-        sc.nextLine();
+            String matriz[][] = new String[filas][columnas];
+            sc.nextLine();
 
-        boolean valido = false;
-        for (int i = 0; i < filas; i++) {
-            System.out.print("Introduce las letras de la fila " + (i + 1) + ": ");
-            String palabra = sc.nextLine();
-            if (!palabra.matches("[a-zA-Z]+") || palabra.length() != columnas){
-                System.out.println("ERROR: Introduce datos validos (" + columnas + " Letras).\n");
-                break;
-            } else {
-                String letras[] = palabra.split("");
-                for (int j = 0; j < columnas; j++) {
-                    matriz[i][j] = letras[j];
-                }
-                valido = true;
-            }
-        }
-
-        if (valido){
-            // region Imprimir matriz
+            boolean valido = false;
             for (int i = 0; i < filas; i++) {
-                for (int j = 0; j < columnas; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.println();
-            }
-            // endregion
-
-            System.out.print("\nIntroduce la palabra a buscar: ");
-            String buscar = sc.nextLine().toLowerCase();
-            if (!buscar.matches("[a-zA-Z]+")){
-                System.out.println("ERROR: Palabra no valida");
-            } else {
-                boolean encontrada = false;
-                // region Horizontal
-                for (int i = 0; i < filas; i++) {
-                    String fila = "";
+                System.out.print("Introduce las letras de la fila " + (i + 1) + ": ");
+                String palabra = sc.nextLine();
+                if (!palabra.matches("[a-zA-Z]+") || palabra.length() != columnas){
+                    System.out.print("ERROR: Introduce datos validos (" + columnas + " Letras).");
+                    valido = false;
+                    break;
+                } else {
+                    String letras[] = palabra.split("");
                     for (int j = 0; j < columnas; j++) {
-                        fila += matriz[i][j].toLowerCase();
-                        if (fila.contains(buscar)) {
-                            encontrada = true;
-                            System.out.println("Encontrada!!! En la posicion " + i + " " + ((buscar.length() - j) + 1));
-                            break;
-                        }
+                        matriz[i][j] = letras[j];
                     }
-                    if (encontrada){
-                        break;
+                    valido = true;
+                }
+            }
+
+            if (valido){
+                // region Imprimir matriz
+                for (int i = 0; i < filas; i++) {
+                    for (int j = 0; j < columnas; j++) {
+                        System.out.print(matriz[i][j] + " ");
                     }
+                    System.out.println();
                 }
                 // endregion
 
-                // region Vertical
-                if (!encontrada) {
-                    for (int j = 0; j < columnas; j++) {
-                        String columna = "";
-                        for (int i = 0; i < filas; i++) {
-                            columna += matriz[i][j].toLowerCase();
-                            if (columna.contains(buscar)) {
+                System.out.print("\nIntroduce la palabra a buscar: ");
+                String buscar = sc.nextLine().toLowerCase();
+                if (!buscar.matches("[a-zA-Z]+")){
+                    System.out.print("ERROR: Palabra no valida");
+                } else {
+                    boolean encontrada = false;
+                    // region Horizontal
+                    for (int i = 0; i < filas; i++) {
+                        String fila = "";
+                        for (int j = 0; j < columnas; j++) {
+                            fila += matriz[i][j].toLowerCase();
+                            if (fila.contains(buscar)) {
                                 encontrada = true;
-                                System.out.println("Encontrada!!! En la posicion " + ((buscar.length() - i) + 1) + " " + j);
+                                System.out.println("Encontrada!!! En la posicion " + i + " " + ((buscar.length() - j) + 1));
                                 break;
                             }
                         }
@@ -77,9 +62,30 @@ public class sopaLetras {
                             break;
                         }
                     }
+                    // endregion
+
+                    // region Vertical
+                    if (!encontrada) {
+                        for (int j = 0; j < columnas; j++) {
+                            String columna = "";
+                            for (int i = 0; i < filas; i++) {
+                                columna += matriz[i][j].toLowerCase();
+                                if (columna.contains(buscar)) {
+                                    encontrada = true;
+                                    System.out.println("Encontrada!!! En la posicion " + ((buscar.length() - i) + 1) + " " + j);
+                                    break;
+                                }
+                            }
+                            if (encontrada){
+                                break;
+                            }
+                        }
+                    }
+                    // endregion
                 }
-                // endregion
             }
+        } catch (InputMismatchException error){
+            System.out.print("ERROR: Valores incorrectos");
         }
     }
 }
