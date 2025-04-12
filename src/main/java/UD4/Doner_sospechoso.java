bc1eb44e486077330fc51485d2df21b58d0d0bb9
Dredux <David-RD@outlook.es>
2025-04-12 23:03:36 +0200

    Adds basic structure for Doner program
    Initializes a basic program structure with a scanner for
    input and a loop to handle multiple test cases. This
    provides a foundation for solving a specific problem.

---
+++ src/main/java/UD4/Doner_sospechoso.java
@@ -0,0 +1,15 @@
+package UD4;
+public class Doner_sospechoso {
+    static java.util.Scanner in;
+    public static void casoDePrueba() {
+        
+    } // casoDePrueba
+
+    public static void main(String[] args) {
+        in = new java.util.Scanner(System.in);
+        int numCasos = in.nextInt();
+        for (int i = 0; i < numCasos; i++) {
+            casoDePrueba();
+        } // main
+    }
+}
